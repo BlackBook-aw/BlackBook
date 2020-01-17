c50ae4882420a07804d55d632f77366da3a8ba52
BlackBook-aw <60007919+BlackBook-aw@users.noreply.github.com>
2020-01-17 23:31:33 +0800

    Add files via upload

---
+++ Links/AdminGate.bat
@@ -0,0 +1,2 @@
+@echo off
+Start "" "%ProgramFiles%\Internet Explorer\iexplore.exe" "https://na-tx-022.asn.saacon.net/"
---
+++ Links/SAACON.bat
@@ -0,0 +1,2 @@
+@echo off
+Start "" "%ProgramFiles%\Internet Explorer\iexplore.exe" "https://iam.saacon.net/"
